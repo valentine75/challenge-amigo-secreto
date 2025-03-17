43b65e8fb3e54cf19aba21db2cce40aa20172c66
Rufina Cruz <rufina_75@hotmail.com>
2025-03-17 02:18:41 -0300

    nuevas funciones

--- app.js
+++ app.js
@@ -3,33 +3,47 @@
 let amigosLista=[];
 let amigoIngresado=document.querySelector("input");
 let listaDeUl=document.getElementById("listaAmigos");
-let ResultadoDeUl=document.getElementById("resultado");
 
-function sortearAmigo(){
-    let sorteo=Math.floor(Math.random()*amigosLista.length);
-    // console.log(amigoSorteado);
-    let amigoSorteado=amigosLista[sorteo];
-    ResultadoDeUl.innerHTML+=`<li>El amigo secreto es: ${amigoSorteado}</li>`;
+function limpiarCaja(){
+    document.querySelector("input").value="";
 }
 
-
-
-
 function agregarAmigos(){
-    if (amigoIngresado.value===""){
+    if (amigoIngresado.value==""){
         alert("Por favor ingrese un nombre");
     }
     else{
         // agregamos a la lista los nombres ingresados por teclado
     amigosLista.push(amigoIngresado.value);
-    console.log(amigosLista);
+    // console.log(amigosLista);
 // usamos innerHTML para actualizar dinamicamente la lista con los nombres ingresados por teclado
     listaDeUl.innerHTML+= `<li>${amigoIngresado.value}</li>`;
 }
+limpiarCaja();
     }
+
+
+
+function sortearAmigo(){
+    let sorteo=Math.floor(Math.random()*amigosLista.length);
+    // console.log(sorteo);   
+    let amigoSorteado=amigosLista[sorteo];
+    let Resultado=document.getElementById("resultado");
+    Resultado.innerHTML=`El amigo sorteado es:${amigoSorteado}`;
+    // DEJAMOS DE MOSTRAR LA lISTA
+    document.getElementById("listaAmigos").innerHTML="";
+    
+    if(amigosLista.length===0){
+        alert("Has completado el sorteo");
+        document.location.reload();
+    }
+    // borramos los nombre sorteados
+    amigosLista.splice(sorteo, 1);
+}
+
+
 
  
 
 // LLAMADA A LAS FUNCIONES
 agregarAmigos();
-sortearAmigo();
